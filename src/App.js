ce2b4fc095a60af09d71d2a490b0fe68206f8791
Blackpanda34 <129926202+Blackpanda34@users.noreply.github.com>
2023-04-05 06:50:00 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -6,19 +6,25 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          Octo Cat is <span className="heart">♥️</span> 
         </p>
         <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
+        “Philosophy is like being in a dark room and looking for a black cat.
+        </p>
+        <p className="small">
+        Metaphysics is like being in a dark room and looking for a black cat that isn’t there. 
+        </p><p className="small">
+        Theology is like being in a dark room and looking for a black cat that isn’t there, and shouting “I found it!”
         </p>
         <p>
+          
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Black Neko
           </a>
         </p>
       </header>
